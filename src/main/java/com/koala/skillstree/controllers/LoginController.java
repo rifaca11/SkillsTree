82d39dbdf8c8e7a217dcea6f5eff935f8875b4f7
rifaca11 <charifah1112@gmail.com>
2022-12-16 11:01:06 +0100

    Build: front login

--- src/main/java/com/koala/skillstree/controllers/LoginController.java
+++ src/main/java/com/koala/skillstree/controllers/LoginController.java
@@ -2,19 +2,19 @@ package com.koala.skillstree.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 //pour definir cette classe comme controller
 @Controller
-@RequestMapping("/Student")
+//@RequestMapping("/student")
 public class LoginController {
 //    pour definir cette function comme lien
-    @GetMapping(value = "/login")
+    @GetMapping(value = "/")
 //    Pour retourner la valeur dans le body
     public String loginPage(){
-        return "KOALA";
+         return "Login";
     }
+
 
 }
